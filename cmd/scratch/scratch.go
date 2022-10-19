e77c8e1a31fc20e0bbbe5f7698e3b315e6e150e8
Ben Moss <ben@mossity.com>
2022-10-19 09:58:04 -0400

    create pods from podspec, upload logs, use emptydir volume

--- cmd/scratch/scratch.go
+++ cmd/scratch/scratch.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"os/signal"
 	"sync"
@@ -128,179 +130,172 @@ func (w *worker) run(ctx context.Context, wg *sync.WaitGroup) {
 				w.logger.Error("start: %v", err)
 			}
 			w.logger.Info("start: %v", litter.Sdump(job))
-			plugins, err := plugin.CreateFromJSON(job.Env["BUILDKITE_PLUGINS"])
+			pod, err := w.podFromJob(job, client)
 			if err != nil {
-				w.logger.Warn("err converting plugins to json: %v", err)
-				var env []corev1.EnvVar
-				for k, v := range job.Env {
-					env = append(env, corev1.EnvVar{Name: k, Value: v})
-				}
-				env = append(env, corev1.EnvVar{
-					Name:  "BUILDKITE_BUILD_PATH",
-					Value: "/buildkite/builds",
-				})
-				env = append(env, corev1.EnvVar{
-					Name:  "BUILDKITE_AGENT_ACCESS_TOKEN",
-					Value: client.Config().Token,
-				})
-				pod, err := w.client.CoreV1().Pods("default").Create(ctx, &corev1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						GenerateName: "agent-",
-					},
-					Spec: corev1.PodSpec{
-						RestartPolicy: corev1.RestartPolicyNever,
-						Containers: []corev1.Container{
-							{
-								Name:  "agent",
-								Image: "buildkite/agent:latest",
-								Args: []string{
-									"bootstrap",
-								},
-								Env: env,
-							},
-						},
-					},
-				}, metav1.CreateOptions{})
-				if err != nil {
-					w.logger.Error("failed to create pod: %v", err)
-					return
-				}
-				w.logger.Info("created pod: %s", pod.Name)
-				fs := fields.OneTermEqualSelector(metav1.ObjectNameField, pod.Name)
-				lw := &cache.ListWatch{
-					ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
-						options.FieldSelector = fs.String()
-						return w.client.CoreV1().Pods(pod.Namespace).List(context.TODO(), options)
-					},
-					WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
-						options.FieldSelector = fs.String()
-						return w.client.CoreV1().Pods("default").Watch(ctx, options)
-					},
-				}
-				_, err = toolswatch.UntilWithSync(ctx, lw, &corev1.Pod{}, nil, func(ev watch.Event) (bool, error) {
-					if pod, ok := ev.Object.(*corev1.Pod); ok {
-						if pod.Status.Phase == corev1.PodSucceeded {
-							w.logger.Info("pod success!")
-							return true, nil
-						}
-						w.logger.Info("pod not success! status: %s", pod.Status.Phase)
-						job.ExitStatus = "0"
-						return false, nil
+				w.logger.Error("podFromJob: %v", err)
+				return
+			}
+			pod, err = w.client.CoreV1().Pods("default").Create(ctx, pod, metav1.CreateOptions{})
+			if err != nil {
+				w.logger.Error("failed to create pod: %v", err)
+				return
+			}
+			w.logger.Info("created pod: %s", pod.Name)
+			fs := fields.OneTermEqualSelector(metav1.ObjectNameField, pod.Name)
+			lw := &cache.ListWatch{
+				ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+					options.FieldSelector = fs.String()
+					return w.client.CoreV1().Pods(pod.Namespace).List(context.TODO(), options)
+				},
+				WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+					options.FieldSelector = fs.String()
+					return w.client.CoreV1().Pods("default").Watch(ctx, options)
+				},
+			}
+			_, err = toolswatch.UntilWithSync(ctx, lw, &corev1.Pod{}, nil, func(ev watch.Event) (bool, error) {
+				if pod, ok := ev.Object.(*corev1.Pod); ok {
+					if pod.Status.Phase == corev1.PodSucceeded {
+						w.logger.Info("pod success!")
+						return true, nil
 					}
-					return false, errors.New("event object not of type v1.Node")
-				})
-				if err != nil {
-					w.logger.Error("failed to watch pod: %v", err)
-					return
+					w.logger.Info("pod not success! status: %s", pod.Status.Phase)
+					job.ExitStatus = "0"
+					return false, nil
 				}
-				if _, err := client.FinishJob(job); err != nil {
-					w.logger.Error("failed to finish job: %v", err)
-					return
-				}
+				return false, errors.New("event object not of type v1.Node")
+			})
+			if err != nil {
+				w.logger.Error("failed to watch pod: %v", err)
+				return
+			}
+			if _, err := client.FinishJob(job); err != nil {
+				w.logger.Error("failed to finish job: %v", err)
+				return
+			}
+			req := w.client.CoreV1().Pods(pod.Namespace).GetLogs(pod.Name, &corev1.PodLogOptions{})
+			podLogs, err := req.Stream(ctx)
+			if err != nil {
+				w.logger.Error("error in opening stream: %v", err)
+				return
+			}
+			defer podLogs.Close()
+
+			buf := new(bytes.Buffer)
+			_, err = io.Copy(buf, podLogs)
+			if err != nil {
+				w.logger.Error("error in copy information from podLogs to buf: %v", err)
+				return
+			}
+			str := buf.String()
+			_, err = client.UploadChunk(job.ID, &api.Chunk{
+				Data:     str,
+				Sequence: 0,
+				Offset:   0,
+				Size:     len(str),
+			})
+			if err != nil {
+				w.logger.Error("upload chunk: %v", err)
+				return
 			}
 
+			if err := w.client.CoreV1().Pods("default").Delete(ctx, pod.Name, metav1.DeleteOptions{}); err != nil {
+				w.logger.Error("delete pod: %v", err)
+				return
+			}
+		}
+	}
+}
+func (w *worker) podFromJob(job *api.Job, client *api.Client) (*corev1.Pod, error) {
+	var pod *corev1.Pod
+	if job.Env["BUILDKITE_PLUGINS"] == "" {
+		w.logger.Warn("no plugins specified, using default bootstrap pod")
+		pod = defaultBootstrapPod
+	} else {
+		plugins, err := plugin.CreateFromJSON(job.Env["BUILDKITE_PLUGINS"])
+		if err != nil {
+			return nil, fmt.Errorf("err converting plugins to json: %w", err)
+		} else {
+			// create regular pod
 			// "BUILDKITE_PLUGINS":                            "[{\"github.com/buildkite-plugins/shellcheck-buildkite-plugin\":{\"files\":[\"hooks/**\",\"lib/**\",\"commands/**\"]}}]",
 			for _, plugin := range plugins {
 				w.logger.Info("plugin: %v", litter.Sdump(plugin))
 				var podSpec corev1.PodSpec
 				asJson, err := json.Marshal(plugin.Configuration)
 				if err != nil {
-					w.logger.Error("failed to marshal config: %v", err)
-					return
+					return nil, fmt.Errorf("failed to marshal config: %w", err)
 				}
 				if err := json.Unmarshal(asJson, &podSpec); err != nil {
-					w.logger.Error("failed to unmarshal config: %v", err)
-					return
+					return nil, fmt.Errorf("failed to unmarshal config: %w", err)
 				}
 				w.logger.Info("podSpec: %v", litter.Sdump(podSpec))
+				pod = &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: fmt.Sprintf("buildkite-%s", job.ID),
+					},
+					Spec: podSpec,
+				}
 			}
-
-			// 	w.logger.Info("accept job: %v", litter.Sdump(job))
-			// 	_, err = client.StartJob(resp.Job)
-			// 	if err != nil {
-			// 		w.logger.Error("start: %v", err)
-			// 	}
-			// 	_, err = client.UploadChunk(job.ID, &api.Chunk{
-			// 		Data:     "heyo",
-			// 		Sequence: 0,
-			// 		Offset:   0,
-			// 		Size:     len("heyo"),
-			// 	})
-			// 	if err != nil {
-			// 		w.logger.Error("upload chunk: %v", err)
-			// return
-			// 	}
-			// 	_, err = client.FinishJob(job)
-			// 	if err != nil {
-			// 		w.logger.Error("finish: %v", err)
-			// return
-			// 	}
 		}
 	}
+	pod.Spec.RestartPolicy = corev1.RestartPolicyNever
+	pod.Spec.InitContainers = append(pod.Spec.InitContainers, corev1.Container{
+		Name:  "bootstrap",
+		Image: "buildkite/agent:latest",
+		Args: []string{
+			"bootstrap", "--phases=checkout",
+		},
+	})
+	pod.Spec.Volumes = append(pod.Spec.Volumes, corev1.Volume{
+		Name: "workspace",
+		VolumeSource: corev1.VolumeSource{
+			EmptyDir: &corev1.EmptyDirVolumeSource{},
+		},
+	})
+	env := make([]corev1.EnvVar, 0, len(job.Env)+2)
+	env = append(env, corev1.EnvVar{
+		Name:  "BUILDKITE_BUILD_CHECKOUT_PATH",
+		Value: "/workspace",
+	}, corev1.EnvVar{
+		Name:  "BUILDKITE_AGENT_ACCESS_TOKEN",
+		Value: client.Config().Token,
+	})
+	for k, v := range job.Env {
+		env = append(env, corev1.EnvVar{Name: k, Value: v})
+	}
+	volumeMounts := []corev1.VolumeMount{{Name: "workspace", MountPath: "/workspace"}}
+	for i, c := range pod.Spec.Containers {
+		c.Env = append(c.Env, env...)
+		if c.Name == "" {
+			c.Name = fmt.Sprintf("container-%d", i)
+		}
+		c.VolumeMounts = append(c.VolumeMounts, volumeMounts...)
+		pod.Spec.Containers[i] = c
+	}
+	for i, c := range pod.Spec.InitContainers {
+		c.Env = append(c.Env, env...)
+		if c.Name == "" {
+			c.Name = fmt.Sprintf("container-%d", i)
+		}
+		c.VolumeMounts = append(c.VolumeMounts, volumeMounts...)
+		pod.Spec.InitContainers[i] = c
+	}
+	return pod, nil
 }
 
-type Kubernetes struct {
-	Name string
-}
-
-var sampleJob = &api.Job{
-	ID:       "0183d1b7-29b2-452b-b308-ac5e69394a28",
-	Endpoint: "https://agent.buildkite.com/v3",
-	State:    "accepted",
-	Env: map[string]string{
-		"BUILDKITE":                                    "true",
-		"BUILDKITE_AGENT_ID":                           "0183d1b8-18fc-4831-bfd2-057763c13a3a",
-		"BUILDKITE_AGENT_META_DATA_QUEUE":              "default",
-		"BUILDKITE_AGENT_META_DATA_ROLE":               "kaniko",
-		"BUILDKITE_AGENT_NAME":                         "bmo",
-		"BUILDKITE_ARTIFACT_PATHS":                     "",
-		"BUILDKITE_BRANCH":                             "master",
-		"BUILDKITE_BUILD_AUTHOR":                       "",
-		"BUILDKITE_BUILD_AUTHOR_EMAIL":                 "",
-		"BUILDKITE_BUILD_CREATOR":                      "Ben Moss",
-		"BUILDKITE_BUILD_CREATOR_EMAIL":                "ben.moss@superorbital.io",
-		"BUILDKITE_BUILD_ID":                           "0183d1b5-03ef-4db7-8b74-60d1e3d67c1f",
-		"BUILDKITE_BUILD_NUMBER":                       "54",
-		"BUILDKITE_BUILD_URL":                          "https://buildkite.com/superorbital/bmo/builds/54",
-		"BUILDKITE_COMMAND":                            "",
-		"BUILDKITE_COMMIT":                             "9f139aaf7f6e6bc66058a0ca0d9fba1495f9375d",
-		"BUILDKITE_JOB_ID":                             "0183d1b7-29b2-452b-b308-ac5e69394a28",
-		"BUILDKITE_LABEL":                              ":shell: Shellcheck",
-		"BUILDKITE_MESSAGE":                            "explore",
-		"BUILDKITE_ORGANIZATION_SLUG":                  "superorbital",
-		"BUILDKITE_PIPELINE_DEFAULT_BRANCH":            "master",
-		"BUILDKITE_PIPELINE_ID":                        "01835c15-60c7-47fc-a5f0-7e91753c853a",
-		"BUILDKITE_PIPELINE_NAME":                      "bmo",
-		"BUILDKITE_PIPELINE_PROVIDER":                  "github",
-		"BUILDKITE_PIPELINE_SLUG":                      "bmo",
-		"BUILDKITE_PLUGINS":                            "[{\"github.com/buildkite-plugins/shellcheck-buildkite-plugin\":{\"files\":[\"hooks/**\",\"lib/**\",\"commands/**\"]}}]",
-		"BUILDKITE_PROJECT_PROVIDER":                   "github",
-		"BUILDKITE_PROJECT_SLUG":                       "superorbital/bmo",
-		"BUILDKITE_PULL_REQUEST":                       "false",
-		"BUILDKITE_PULL_REQUEST_BASE_BRANCH":           "",
-		"BUILDKITE_PULL_REQUEST_REPO":                  "",
-		"BUILDKITE_REBUILT_FROM_BUILD_ID":              "",
-		"BUILDKITE_REBUILT_FROM_BUILD_NUMBER":          "",
-		"BUILDKITE_REPO":                               "https://github.com/benmoss/docker-compose-buildkite-plugin",
-		"BUILDKITE_RETRY_COUNT":                        "0",
-		"BUILDKITE_SCRIPT_PATH":                        "",
-		"BUILDKITE_SOURCE":                             "ui",
-		"BUILDKITE_STEP_ID":                            "0183d1b7-2983-41c3-bf89-88e444aae4ab",
-		"BUILDKITE_STEP_KEY":                           "",
-		"BUILDKITE_TAG":                                "",
-		"BUILDKITE_TIMEOUT":                            "false",
-		"BUILDKITE_TRIGGERED_FROM_BUILD_ID":            "",
-		"BUILDKITE_TRIGGERED_FROM_BUILD_NUMBER":        "",
-		"BUILDKITE_TRIGGERED_FROM_BUILD_PIPELINE_SLUG": "",
-		"CI": "true",
+var defaultBootstrapPod = &corev1.Pod{
+	ObjectMeta: metav1.ObjectMeta{
+		GenerateName: "agent-",
 	},
-	ChunksMaxSizeBytes: 102400,
-	Token:              "",
-	ExitStatus:         "",
-	Signal:             "",
-	SignalReason:       "",
-	StartedAt:          "",
-	FinishedAt:         "",
-	RunnableAt:         "2022-10-13T14:19:45.482Z",
-	ChunksFailedCount:  0,
+	Spec: corev1.PodSpec{
+		RestartPolicy: corev1.RestartPolicyNever,
+		Containers: []corev1.Container{
+			{
+				Name:  "agent",
+				Image: "buildkite/agent:latest",
+				Args: []string{
+					"bootstrap",
+				},
+			},
+		},
+	},
 }
